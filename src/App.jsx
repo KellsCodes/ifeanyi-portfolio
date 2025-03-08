4835492bb1d8236a6ab7d2c7709e13821e763f34
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-03-08 07:16:12 +0100

    Add Layout element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/logo.webp'
 import './App.css'
+import Layout from './layout/Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Layout />
     </>
   )
 }
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,7 @@
+function Layout() {
+    return (
+        <div className="bg-white mx-auto h-[100vh] w-[62vw]"></div>
+    )
+}
+
+export default Layout;
