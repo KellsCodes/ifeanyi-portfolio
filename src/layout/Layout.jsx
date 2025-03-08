07963fee608cc6430cbfce090f6e303da075f398
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-03-08 21:08:46 +0100

    complete hero section desktop

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 function Layout({ children }) {
     return (
-        <div className="bg-white mx-auto h-[250vh] w-[62vw] px-25">
+        <div className="bg-white mx-auto w-[62vw] px-25">
             <Navbar />
             {/* <HeroSection /> */}
             {children}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,33 @@
 import HeroSection from "../components/Hero";
 import Layout from "../layout/Layout";
+import { BsLinkedin } from "react-icons/bs";
+import { IoLogoGithub } from "react-icons/io";
+import { FaFacebookSquare, FaMailBulk } from "react-icons/fa";
+import { FaFreeCodeCamp } from "react-icons/fa";
 
 export default function Home() {
     return (
         <Layout>
             <HeroSection />
-            <div className="mt-3 w-full">
+            <div className="mt-5 w-full">
                 <div className="flex items-center relative">
-                    <section className="w-[70%] pr-24 ">
-                        <h1 className="font-[700] text-[45px] text-primary">Full-stack web developer with backend focus</h1>
-                        <p>
+                    <section className="w-[70%] pr-24 space-y-8">
+                        <h1 className="font-[700] text-[40px] leading-12 text-primary font-secondary">Full-stack web developer with backend focus</h1>
+                        <p className="leading-9 font-medium font-primary text-secondary text-md">
                             I am a full-stack developer (backend focus) with expertise in Django, Node.js, and React.js. I have experience building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.
                         </p>
                     </section>
-                    <div className="absolute h-[340px] w-[320px] rotate-3 rounded-[20px] p-[1px] right-0 drop-shadow-lg">
+                    <div className="absolute h-[350px] w-[320px] rotate-3 rounded-[20px] p-[1px] right-0 drop-shadow-lg">
                         <img src="/personal_logo.webp" alt="bg-img" loading="lazy" className="h-[100%] w-[100%] rounded-[20px] object-cover" />
                     </div>
+                </div>
+
+                <div className="h-20 flex items-center gap-x-5 text-primary opacity-80 text-[25px]">
+                    <a href="https://www.linkedin.com/in/ifeanyi-nworji-619222b7" target="_blank"><BsLinkedin /></a>
+                    <a href="https://github.com/KellsCodes" target="_blank"><IoLogoGithub /></a>
+                    <a href="https://web.facebook.com/ifeanyi.nworji.7/" target="_blank"><FaFacebookSquare /></a>
+                    <a href="mailto:nworjiifeanyi@gmail.com" target="_blank"><FaMailBulk /></a>
+                    <a href="https://www.freecodecamp.org/kellscode" target="_blank"><FaFreeCodeCamp /></a>
                 </div>
 
             </div>
