d865fa7e6796c44d8b5ef857b8ad9053f578b97e
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-03-08 08:31:16 +0100

    Setup Nabar

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import Error404 from './pages/ErrorPage'
+import routes from './routes/routes'
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.about} element={<About />} />
+        <Route path={routes.projects} element={<Projects />} />
+        <Route path={routes.contact} element={<Contact />} />
         <Route path='*' element={<Error404 />} />
       </Routes>
     </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,38 @@
 import { Link } from "react-router-dom"
+import { useLocation } from "react-router-dom"
+import routes from "../routes/routes"
 
 function Navbar() {
+    const location = useLocation()
     return (
-        <nav className="flex items-center gap-x-5">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
+        <nav className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-x-7 fixed w-80 top-[30px] h-10 rounded-full p-2 shadow font-semibold text-sm">
+                <Link
+                    to={routes.home}
+                    className={`${location.pathname === routes.home ? "text-link" : ""}`}
+                >
+                    Home
+                </Link>
+                <Link
+                    to={routes.about}
+                    className={`${location.pathname === routes.about ? "text-link" : ""}`}
+                >
+                    About
+                </Link>
+                <Link
+                    to={routes.projects}
+                    className={`${location.pathname === routes.projects ? "text-link" : ""}`}
+                >
+                    Projects
+                </Link>
+                <Link
+                    to={routes.contact}
+                    className={`${location.pathname === routes.contact ? "text-link" : ""}`}
+                >
+                    Contact
+                </Link>
+
+            </div>
         </nav>
     )
 }
