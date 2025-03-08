b60a79c4fae01ead1c4a16ca6606c3da5c01be98
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-03-08 21:11:50 +0100

    Home section optimized on desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,13 @@ export default function Home() {
             <HeroSection />
             <div className="mt-5 w-full">
                 <div className="flex items-center relative">
-                    <section className="w-[70%] pr-24 space-y-8">
+                    <section className="w-[65%] pr-24 space-y-8">
                         <h1 className="font-[700] text-[40px] leading-12 text-primary font-secondary">Full-stack web developer with backend focus</h1>
                         <p className="leading-9 font-medium font-primary text-secondary text-md">
                             I am a full-stack developer (backend focus) with expertise in Django, Node.js, and React.js. I have experience building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.
                         </p>
                     </section>
-                    <div className="absolute h-[350px] w-[320px] rotate-3 rounded-[20px] p-[1px] right-0 drop-shadow-lg">
+                    <div className="absolute h-[370px] w-[340px] rotate-3 rounded-[20px] p-[1px] right-0 drop-shadow-lg">
                         <img src="/personal_logo.webp" alt="bg-img" loading="lazy" className="h-[100%] w-[100%] rounded-[20px] object-cover" />
                     </div>
                 </div>
